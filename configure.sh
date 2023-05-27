88343fc7e7ad9505be58cf15962a93785521d099
EdvardSire <edvardsire@protonmail.com>
2023-05-27 22:10:02 +0200

    absolute path linking and mv instead of rm

--- configure.sh
+++ configure.sh
@@ -1,22 +1,26 @@
+choice=""
 echo
 echo "You are about to install dotfiles."
 echo "For correct installation you have to be inside the repo."
 echo "Relevant configuration files in .config will be overwitten. "
-read -p "Are you Sure [y]?" -n 1 -r -s
+read -p "Are you Sure [y]?" -s -n1  choice 
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+if [[ $choice =~ ^[Yy]$ ]]; then
 	if [ ! -d "~/.config" ]; then
     mkdir ~/.config >/dev/null 2>&1
 	fi
-  source_repo_path="${PWD}"
+  source_repo_path="${PWD}/"
+  config_path="$HOME/.config/"
 
   for local_directory_name in *; do
-    cd ~/.config
     if [ -d "$local_directory_name" ]; then
-      read -p "Do you want to copy ${local_directory_name} [y]?" -n 1 -r -s
+      choice=""
+      read -p "Do you want to copy ${local_directory_name} [y]?" -s -n1 choice
       echo
-      if [[ $REPLY =~ ^[Yy]$ ]]; then
-        # ln -s "$source_repo_path/$local_directory_name" .
+      if [[ $choice =~ ^[Yy]$ ]]; then
+        # mv "$config_path$local_directory_name" "${config_path}old_$local_directory_name"
+        # ln -s "$source_repo_path$local_directory_name" "$config_path$local_directory_name"
         echo i dont do anything
       fi
     fi
